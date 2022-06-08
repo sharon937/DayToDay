da488bdef184a7480ee6c783da46c929fed89a09
sharon <sharonsnowman521@gmail.com>
2022-06-08 20:30:58 +0800

    todo

--- scr/component/Todolist.js
+++ scr/component/Todolist.js
@@ -1,109 +1,139 @@
-import React from 'react';
-import { ScrollView, Input, AspectRatio, Image, IconButton, Divider, Text, Box, VStack, HStack,useColorMode,  Icon, Center, useToast, NativeBaseProvider } from "native-base";
-import { useState } from 'react/cjs/react.production.min';
-import {Ionicons,AntDesign } from "@expo/vector-icons"
-import {lighttheme,darktheme} from "../theme/Modetheme"
+import React,{ useState } from 'react';
+import { StyleSheet, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard} from 'react-native';
+import { ScrollView, Input, AspectRatio, Image, IconButton, Divider, Text, Box, VStack, HStack, Heading, Icon, Center, useToast, NativeBaseProvider, Pressable, View,useColorMode } from "native-base";
+import {Ionicons,AntDesign } from "@expo/vector-icons";
+import Task from './task';
+import {lighttheme,darktheme} from "../theme/Modetheme";
 
 
 const Todolist = () => {
-  const instState = [{
-    title: "Homework",
-    isCompleted: false
-  }, {
-    title: "Meeting ",
-    isCompleted: false
-  }, {
-    title: "Garbage",
-    isCompleted: false
-  }, {
-    title: "Clean up",
-    isCompleted: false
-  }];
-  const [list, setList] = React.useState(instState);
-  const [inputValue, setInputValue] = React.useState("");
-  const toast = useToast();
-
-  const addItem = title => {
-    if (title === "") {
-      toast.show({
-        title: "Add New List",
-        status: "warning"
-      });
-      return;
-    }
-
-    setList(prevList => {
-      return [...prevList, {
-        title: title,
-        isCompleted: false
-      }];
-    });
-  };
-
-  const handleDelete = index => {
-    setList(prevList => {
-      const temp = prevList.filter((_, itemI) => itemI !== index);
-      return temp;
-    });
-  };
-
-  const handleStatusChange = index => {
-    setList(prevList => {
-      const newList = [...prevList];
-      newList[index].isCompleted = !newList[index].isCompleted;
-      return newList;
-    });
-  };
+ 
   const { colorMode } = useColorMode();
   const Mytheme = colorMode == 'light' ? lighttheme : darktheme;
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
 
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const deleteTask = (deleteIndex) => {
+    setTaskItems(taskItems.filter((value, index) => index != deleteIndex));
+  }
  
 
   return (
     <Box theme={Mytheme}>
-    <Center  zIndex={-99}  _dark={{ bg: "primary.100" }}
+    <Box  zIndex={-99}  _dark={{ bg: "primary.100" }}
                     _light={{ bg: "primary.light" }} >
         <Box  background={"#EDEFF2"}w="324" h="400" padding="2" mt={"100"} mx="auto"  borderRadius={15} shadow={3}>
 
-          <Text pt={30} fontSize="24" color='#1D2942' textAlign="center" mb="5">To Do List</Text>
-          <VStack space={5} ml="5">
-            {list.map((item, itemI) => 
-            <HStack w="100%" justifyContent="space-between" alignItems="center" key={item.title + itemI.toString()}>
-                <IconButton icon={<Icon as={Ionicons} name="moon-outline" />} isChecked={item.isCompleted} 
-                onPress={() => handleStatusChange(itemI)} value={item.title} _icon={{
-                  color: "#1D2942"}}/>
-                  <VStack w="100%">
-                     <Text width="100%" fontSize={20} flexShrink={1} textAlign="left" mx="2" strikeThrough={item.isCompleted} _light={{
-                      color: item.isCompleted ? "gray.400" : "gray.400"
-                      }} _dark={{
-                      color: item.isCompleted ? "gray.400" : "gray.400"
-                      }} onPress={() => handleStatusChange(itemI)}>
-                      {item.title}
-                    </Text>
-                    <Divider bg="#1D2942" w="200"/>
-                  </VStack>
+          
+          <Box bg="#F9FBFF" w="100%" h="100%">
+           <ScrollView >
+          
+              <Box bg="#EDEFF2" w="324" h="400" padding="2"  mx="auto" /* borderRadius={15} shadow={3}*/>
+                <Center>
+              <Text pt={30} fontSize="24" color='#1D2942' textAlign="center" >To Do List</Text>
+              </Center>
+                <View style={styles.item}>
+
+                {
+                  taskItems.map((item, index) => {
+                    return (
+                      <View key={index} style={styles.taskContainer}>
+                        <Task index={index + 1} text={item} deleteTask={() => deleteTask(index)}/>
+                      </View>
+                    )
                   
-              </HStack>)}
- 
-          </VStack>
-  
-            
-            <IconButton icon={<Icon as={AntDesign} name="pluscircleo" size="6" mt="5"/>} onPress={() => {
-            addItem(inputValue);
-            setInputValue("");}} _icon={{color: "#1D2942"}}/>
-         
+                  })
+                }
+
+                {/*<Task text={'Task 1'}/>
+                <Task text={'Task 2'}/> */}
+              </View>
+              
+              <KeyboardAvoidingView
+                  behavior={Platform.OS === "android" ? "padding" : "height"}
+                  style={styles.writeTaskWrapper}
+                >
+                  <TextInput style={styles.input} placeholder={'Add a task'} value={task} onChangeText={text => setTask(text)}/>
+
+                  <TouchableOpacity onPress={() => handleAddTask()}>
+                    <View style={styles.addTask}>
+                      <Text style={styles.add}>+</Text>
+                    </View>
+                  </TouchableOpacity>
+                </KeyboardAvoidingView>
+
+              
+              
+                
+              </Box>    
+              
+      </ScrollView> 
+     
+    </Box>
         </Box>    
         <AspectRatio w="100%" mb="0">
-          <Image
-            source={{uri:'https://raw.githubusercontent.com/leeecch/mid/main/Slice%201.png'}}
-            alt='cloud'
-          />
+                <Image
+                  source={{uri:'https://raw.githubusercontent.com/leeecch/mid/main/Slice%201.png'}}
+                  alt='cloud'
+                />
         </AspectRatio> 
-    </Center>
+    </Box>
+    
     </Box>
    
 
   );
 }
 
+const styles=StyleSheet.create({
+  title:{
+    paddingTop:30,
+    fontSize:24, 
+    color:'#1D2942', 
+    textAlign:'center',
+    marginBottom:24
+  },
+  item: {
+    
+
+  },
+  writeTaskWrapper:{
+    position:'absolute',
+    bottom: 20,
+    width:'90%',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    marginLeft:25,
+    marginRight:25
+  },
+  input:{
+    paddingVertical:12,
+    paddingHorizontal:15,
+    backgroundColor:'#F9FBFF',
+    borderRadius:60,
+    borderColor:'#1D2942',
+    borderWidth:1,
+    width:230,
+  },
+  addTask:{
+    width:28,
+    height:28,
+    borderRadius:60,
+    justifyContent:'center',
+    alignItems:'center',
+    borderColor:'#1D2942',
+    borderWidth:1,
+  },
+  add:{
+
+  }
+
+});
 export default Todolist;
---
+++ scr/component/task.js
@@ -0,0 +1,82 @@
+import React,{ useState } from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image}from 'react-native';
+import { AntDesign } from "@expo/vector-icons";
+
+
+const Task = (props) =>{
+
+    const [check,updatecheck]=useState(false)
+
+    return(
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+            <TouchableOpacity  
+            onPress={()=>updatecheck(!check)}
+          >
+            {check?
+              <Image
+              style={styles.moon}
+              source={{uri: 'https://raw.githubusercontent.com/leeecch/final/main/Property%201%3Ddone.png'}}/>
+              :<Image
+              style={styles.moon}
+              source={{uri: 'https://raw.githubusercontent.com/leeecch/final/main/Property%201%3DDefault.png'}}/>
+              
+            }
+            
+          </TouchableOpacity>
+
+                <View style={styles.textline}>
+                    <Text style={styles.itemText}>{props.text}</Text>
+                    <Text style={styles.line}> ──────────────────── </Text>
+                </View>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <AntDesign style={styles.delete} name="delete" size={20} color='#fff' />
+                </TouchableOpacity>
+                
+            </View>
+           
+
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item:{
+        width:304,
+        padding:5,
+        paddingLeft:18,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginBottom: 10,
+    },
+    itemLeft:{
+        flexDirection:'row',
+        alignItems:'center',
+        flexWrap:'wrap',
+    },
+    moon:{
+        width:24,
+        height:24,
+        color:'#1D2942',
+        paddingRight:10,
+    },
+    textline:{
+        flexDirection:'column',
+    },
+    itemText:{
+        maxWidth:'80%',
+        paddingLeft:8,
+        fontSize:14
+    },
+    line:{
+        color:'#1D2942'
+    },
+    delete:{
+        paddingLeft:8,
+        color:'lightslategrey'
+    }
+
+});
+
+export default Task;
